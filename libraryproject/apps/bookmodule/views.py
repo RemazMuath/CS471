5579736e0ec3f1653f2d9c21f4ac1e45f671ff47
RemazMuath <Rim2zalodah@gmail.com>
2025-03-17 13:41:51 +0300

    lab7

--- libraryproject/apps/bookmodule/views.py
+++ libraryproject/apps/bookmodule/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Book
+
 # def index(request):
 #     return HttpResponse("Hello, world!")
-from django.shortcuts import render 
 
 # def index(request):
 #     name = request.GET.get("name") or "world!"
@@ -76,3 +75,16 @@ def get_books_list():
     book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
     book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
     return [book1, book2, book3]
+
+
+def simple_query(request):
+    mybooks=Book.objects.filter(title__icontains='and') # <- multiple objects
+    return render(request, 'bookmodule/bookList2.html', {'books':mybooks})
+
+def complex_query(request):
+    mybooks=books=Book.objects.filter(author__isnull = False).filter(title__icontains='and').filter(edition__gte = 2).exclude(price__lte = 100)[:10]
+    if len(mybooks)>=1:
+        return render(request, 'bookmodule/bookList2.html', {'books':mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')
+
